b9ae7b26c40e61dc057896db43e181226d899fd1
saarg <milsonneau.jean@free.fr>
2017-10-09 15:51:35 -0400

    Follow target IA

--- Assets/Scripts/EnemyIA.cs
+++ Assets/Scripts/EnemyIA.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : Living {
+public class EnemyIA : Living {
 
 	[SerializeField]
 	private float _gravity = 20.0F;
@@ -10,6 +10,8 @@ public class PlayerController : Living {
 	private Vector3 _moveDirection = Vector3.zero;
 	[SerializeField]
 	private CharacterController _controller;
+	[SerializeField]
+	private Transform _target;
 
 	void Start(){
 		_controller = GetComponent<CharacterController>();
@@ -17,19 +19,26 @@ public class PlayerController : Living {
 
 	void Update() 
 	{
-		Vector3 LookAt = new Vector3(Input.GetAxisRaw("RHorizontal"), 0, -Input.GetAxisRaw("RVertical"));
+		Vector3 LookAt = _target.position - transform.position;
+
 		if (LookAt != Vector3.zero)
 			transform.rotation = Quaternion.LookRotation(LookAt);
-
 		if (_controller.isGrounded) 
 		{
-			_moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			//moveDirection = transform.TransformDirection(moveDirection);
-			_moveDirection *= _speed;
+			if (LookAt.magnitude > 3f) {
+				LookAt.Normalize ();
+				_moveDirection = LookAt;
+				_moveDirection *= _speed;
+			} else if (LookAt.magnitude < 2.5f) {
+				LookAt.Normalize ();
+				_moveDirection = -LookAt;
+				_moveDirection *= _speed;
+			} else {
+				_moveDirection = Vector3.zero;
+			}
 		}
 
 		_moveDirection.y -= _gravity * Time.deltaTime;
 		_controller.Move(_moveDirection * Time.deltaTime);
-		
 	}
 }
---
+++ Assets/Scripts/Living.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Living : MonoBehaviour {
+
+	[SerializeField]
+	protected float _life = 100.0F;
+	[SerializeField]
+	protected float _maxLife = 100.0F;
+	[SerializeField]
+	protected float _speed = 6.0F;
+
+
+}
