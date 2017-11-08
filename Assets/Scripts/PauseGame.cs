49dc7501535af3058470eea5c2684851d27f559b
Erwann <erwann.large@laposte.net>
2017-11-07 20:53:40 -0500

    Version améliorée du Pause Gui (pour le voir, aller dans la scène Erwann 2, sinon, il faut utiliser le préfab et configurer les fonctions des boutons)

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -1,25 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class PauseGame : MonoBehaviour {
+public class PauseGame : MonoBehaviour
+{
     public Transform canvas;
- 
+    public Transform pauseMenu;
+    public Transform soundsMenu;
+    public Transform videoSettingsMenu;
+    public Transform controlsMenu;
+    public Transform Player;
 
 
 
-	
-	// Update is called once per frame
-	void Update () {
-	    if (Input.GetKeyDown("p"))
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
         {
             Pause();
         }
-	}
+    }
     public void Pause()
     {
         if (canvas.gameObject.activeInHierarchy == false)
         {
+            if (pauseMenu.gameObject.activeInHierarchy == false)
+            {
+                pauseMenu.gameObject.SetActive(true);
+                soundsMenu.gameObject.SetActive(false);
+                videoSettingsMenu.gameObject.SetActive(false);
+                controlsMenu.gameObject.SetActive(false);
+            }
             canvas.gameObject.SetActive(true);
             Time.timeScale = 0;
             AudioListener.volume = 0;
@@ -29,6 +41,50 @@ public class PauseGame : MonoBehaviour {
             canvas.gameObject.SetActive(false);
             Time.timeScale = 1;
             AudioListener.volume = 1;
+        }
+    }
+
+    public void Sounds(bool Open)
+    {
+        if (Open)
+        {
+            soundsMenu.gameObject.SetActive(true);
+            pauseMenu.gameObject.SetActive(false);
+        }
+        if (!Open)
+        {
+            soundsMenu.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(true);
+        }
+    }
+
+
+    public void VideoSettings(bool Open)
+    {
+        if (Open)
+        {
+            videoSettingsMenu.gameObject.SetActive(true);
+            pauseMenu.gameObject.SetActive(false);
+        }
+        if (!Open)
+        {
+            videoSettingsMenu.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(true);
+        }
+    }
+
+
+    public void Controls(bool Open)
+    {
+        if (Open)
+        {
+            controlsMenu.gameObject.SetActive(true);
+            pauseMenu.gameObject.SetActive(false);
+        }
+        if (!Open)
+        {
+            controlsMenu.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(true);
         }
     }
 }
