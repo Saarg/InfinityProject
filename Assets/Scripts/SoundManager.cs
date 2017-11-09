4a988de52b7ea59f7736c6fef21bbabb71fa2b5c
Erwann <erwann.large@laposte.net>
2017-11-09 18:51:30 -0500

    Le bouton Quit dans le menu pause renvois maintenant au main menu.

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public float volume=1.0F; 
+    void Start()
+    {
+        AudioListener.volume = volumeSlider.value* AudioListener.volume;
+        volume = AudioListener.volume;
+    }
+
+    void Update()
+    {
+        AudioListener.volume = volumeSlider.value * AudioListener.volume;
+        volume = AudioListener.volume;
+    }
+
+    void OnValueChanged()
+    {
+        AudioListener.volume = volumeSlider.value * AudioListener.volume;
+        volume = AudioListener.volume;
+    }
+}
