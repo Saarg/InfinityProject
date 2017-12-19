c082c62debcb130ebde7aeba4c067ee300545382
saarg <milsonneau.jean@free.fr>
2017-12-19 14:38:03 -0500

    WIP level2 boss

---
+++ Assets/Plugins/RootMotion/FinalIK/IK Solvers/TwistRelaxer.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RootMotion.FinalIK {
+
+	/// <summary>
+	/// Relaxes the twist rotation of this Transform relative to a parent and a child Transform, using their initial rotations as the most relaxed pose.
+	/// </summary>
+	public class TwistRelaxer : MonoBehaviour {
+		
+		[Tooltip("The weight of relaxing the twist of this Transform")] 
+		[Range(0f, 1f)] public float weight = 1f;
+
+		[Tooltip("If 0.5, this Transform will be twisted half way from parent to child. If 1, the twist angle will be locked to the child and will rotate with along with it.")]
+		[Range(0f, 1f)] public float parentChildCrossfade = 0.5f;
+
+		/// <summary>
+		/// Rotate this Transform to relax it's twist angle relative to the "parent" and "child" Transforms.
+		/// </summary>
+		public void Relax() {
+			if (weight <= 0f) return; // Nothing to do here
+			
+			// Find the world space relaxed axes of the parent and child
+			Vector3 relaxedAxisParent = parent.rotation * axisRelativeToParentDefault;
+			Vector3 relaxedAxisChild = child.rotation * axisRelativeToChildDefault;
+			
+			// Cross-fade between the parent and child
+			Vector3 relaxedAxis = Vector3.Slerp(relaxedAxisParent, relaxedAxisChild, parentChildCrossfade);
+			
+			// Convert relaxedAxis to (axis, twistAxis) space so we could calculate the twist angle
+			Quaternion r = Quaternion.LookRotation(transform.rotation * axis, transform.rotation * twistAxis);
+			relaxedAxis = Quaternion.Inverse(r) * relaxedAxis;
+			
+			// Calculate the angle by which we need to rotate this Transform around the twist axis.
+			float angle = Mathf.Atan2(relaxedAxis.x, relaxedAxis.z) * Mathf.Rad2Deg;
+			
+			// Store the rotation of the child so it would not change with twisting this Transform
+			Quaternion childRotation = child.rotation;
+			
+			// Twist the bone
+			transform.rotation = Quaternion.AngleAxis(angle * weight, transform.rotation * twistAxis) * transform.rotation;
+			
+			// Revert the rotation of the child
+			child.rotation = childRotation;
+		}
+
+		private Vector3 twistAxis = Vector3.right;
+		private Vector3 axis = Vector3.forward;
+		private Vector3 axisRelativeToParentDefault, axisRelativeToChildDefault;
+		private Transform parent;
+		private Transform child;
+
+		void Start() {
+			parent = transform.parent;
+			if (transform.childCount == 0) {
+				Debug.LogError("The Transform of a TwistRelaxer has no children. Can not use TwistRelaxer on that bone.");
+				return;
+			}
+			child = transform.GetChild(0);
+
+			twistAxis = transform.InverseTransformDirection(child.position - transform.position);
+			axis = new Vector3(twistAxis.y, twistAxis.z, twistAxis.x);
+
+			// Axis in world space
+			Vector3 axisWorld = transform.rotation * axis;
+
+			// Store the axis in worldspace relative to the rotations of the parent and child
+			axisRelativeToParentDefault = Quaternion.Inverse(parent.rotation) * axisWorld;
+			axisRelativeToChildDefault = Quaternion.Inverse(child.rotation) * axisWorld;
+		}
+
+		void LateUpdate() {
+			Relax();
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy/SpiderBoss.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Weapons;
+
+public class SpiderBoss : MonoBehaviour { 
+	[SerializeField]
+	private GameObject _target;
+
+	[Header("Arms")]
+	[SerializeField]
+	private Transform LArmTarget;
+	[SerializeField]
+	private Transform RArmTarget;
+	[SerializeField]
+	private AnimationCurve RotateY;
+	[SerializeField]
+	private AnimationCurve AttackY;
+	[SerializeField]
+	private AnimationCurve AttackZ;
+	private bool LArmBusy;
+	private bool RArmBusy;
+
+	[Header("gun arms")]
+	[SerializeField]
+	private Vector3 gunOffset;
+	[SerializeField]
+	private Transform LGunArmTarget;
+	[SerializeField]
+	private Transform RGunArmTarget;
+
+	public LayerMask layerMask;
+
+	[Header("Weapons")]
+	[SerializeField]
+	private Weapon leftGun;
+	[SerializeField]
+	private Weapon rightGun;
+
+	// Use this for initialization
+	void Start () {
+		_target = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Look At
+		if (Vector3.Angle(transform.forward, _target.transform.position - transform.position) > 10f) {
+			Quaternion startRotation = transform.rotation;
+			transform.LookAt (_target.transform.position);
+			Quaternion targetRotation = transform.rotation;
+
+			transform.rotation = Quaternion.Lerp (startRotation, targetRotation, Time.deltaTime);
+		}
+
+		// Handle arms
+		{
+			Vector3 startRayPos = (transform.position + transform.up + transform.forward);
+
+			// Handle left arms
+			RaycastHit hit;
+			Vector3 rayDir = LArmTarget.position - startRayPos;
+			Ray ray = new Ray (startRayPos, rayDir);
+			if (Physics.Raycast (ray, out hit, rayDir.magnitude, layerMask)) {
+				LArmTarget.position = hit.point;
+			}
+
+			// Handle right arms
+			rayDir = RArmTarget.position - (startRayPos);
+			ray = new Ray (startRayPos, rayDir);
+			if (Physics.Raycast (ray, out hit, rayDir.magnitude, layerMask)) {
+				RArmTarget.position = hit.point;
+			}
+
+			// Handle left gun arms
+			Vector3 offset = gunOffset;
+			LGunArmTarget.position = startRayPos + (_target.transform.position + offset - startRayPos)/2;
+
+			// Handle right gun arms
+			offset = Vector3.Scale(offset, new Vector3(-1, 1, 1));
+			RGunArmTarget.position = startRayPos + (_target.transform.position + offset - startRayPos)/2;
+		}
+
+		leftGun.transform.LookAt (_target.transform.position);
+		leftGun.Fire ();
+
+		rightGun.transform.LookAt (_target.transform.position);
+		rightGun.Fire ();
+
+		if ((_target.transform.position - transform.position).magnitude < 7f && !LArmBusy && !RArmBusy) {
+			StartCoroutine (LegAttack ());
+		} else if (Vector3.Angle(transform.forward, _target.transform.position - transform.position) > 10f && !LArmBusy && !RArmBusy) {
+			StartCoroutine (LegWalk ());
+		}
+	}
+
+	IEnumerator LegWalk() {
+		if (!LArmBusy && !RArmBusy) {
+			LArmBusy = true;
+			RArmBusy = true;
+
+			float startTime = Time.realtimeSinceStartup;
+			Vector3 LstartPos = LArmTarget.localPosition;
+			Vector3 RstartPos = RArmTarget.localPosition;
+
+			float time = Time.realtimeSinceStartup - startTime;
+			while (time < 1f) {
+				LArmTarget.localPosition = LstartPos + Vector3.up * RotateY.Evaluate (Time.realtimeSinceStartup % 1f);
+				RArmTarget.localPosition = RstartPos + Vector3.up * RotateY.Evaluate ((Time.realtimeSinceStartup + 0.5f) % 1f);
+
+				yield return new WaitForSeconds (0.01f);
+				time = Time.realtimeSinceStartup - startTime;
+			}
+
+			LArmTarget.localPosition = LstartPos;
+			RArmTarget.localPosition = RstartPos;
+
+			LArmBusy = false;
+			RArmBusy = false;
+		}
+	}
+
+	IEnumerator LegAttack() {
+		Transform target = null;
+
+		// Choose wich leg should attack
+		float angle = Vector3.SignedAngle(transform.forward, _target.transform.position - transform.position, Vector3.up);
+		if (!LArmBusy && angle < 0) {
+			LArmBusy = true;
+
+			target = LArmTarget;
+		} else if (!RArmBusy && angle > 0) {
+			RArmBusy = true;
+
+			target = RArmTarget;
+		} 
+
+		if (target != null) {
+			Debug.Log ("Attacking!");
+
+			float startTime = Time.realtimeSinceStartup;
+			Vector3 startWorlPos = target.position;
+			Vector3 startLocaPos = target.localPosition;
+
+			float time = Time.realtimeSinceStartup - startTime;
+			while (time < 1f) {
+				Debug.DrawLine (startWorlPos, 
+					startWorlPos + (_target.transform.position - startWorlPos) * AttackZ.Evaluate (time / 1f), 
+					Color.red, 0.05f);
+
+				target.position = startWorlPos;
+				target.position += (_target.transform.position - startWorlPos) * AttackZ.Evaluate (time / 1f);
+				target.position += Vector3.up * AttackY.Evaluate (time / 1f);
+
+				yield return new WaitForSeconds (0.05f);
+				time = Time.realtimeSinceStartup - startTime;
+			}
+
+			startTime = Time.realtimeSinceStartup;
+			time = Time.realtimeSinceStartup - startTime;
+
+			while (time < 1f) {
+				target.localPosition = Vector3.Lerp (target.localPosition, startLocaPos, time / 1f);
+
+				yield return new WaitForSeconds (0.05f);
+				time = Time.realtimeSinceStartup - startTime;
+			}
+
+			target.localPosition = startLocaPos;
+
+			LArmBusy = target == LArmTarget ? false : LArmBusy;
+			RArmBusy = target == RArmTarget ? false : RArmBusy;
+		}
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.green;
+
+		Gizmos.DrawWireSphere (transform.position, 7f);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere (LArmTarget.transform.position, 0.1f);
+		Gizmos.DrawWireSphere (RArmTarget.transform.position, 0.1f);
+		Gizmos.DrawWireSphere (LGunArmTarget.transform.position, 0.1f);
+		Gizmos.DrawWireSphere (RGunArmTarget.transform.position, 0.1f);
+	}
+}
